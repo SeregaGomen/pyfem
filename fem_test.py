79a3b0e3c8eae9496ad0b823a28f7fc41ebc68aa
SeregaGomen <gserega71@gmail.com>
2016-01-27 08:19:45 +0200

    Last

--- fem_test.py
+++ fem_test.py
@@ -245,21 +245,57 @@ def console_dynamic():
 
 if __name__ == "__main__":
     # beam()
-    console_dynamic()
+    # console_dynamic()
+    # head3d()
+    # body1d()
+    # cube()
+    # console()
+    # tank3()
+    # cylinder()
+    # quad()
+    # console4()
+    # cube_test()
+    # console_dynamic()
 
-# head3d()
-# body1d()
-# cube()
-# console()
-# tank3()
-# cylinder()
-# quad()
-# console4()
-# cube_test()
-# console_dynamic()
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib import cm
+    from matplotlib.ticker import LinearLocator
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    X = np.arange(-5, 5, 0.25)
+    xlen = len(X)
+    Y = np.arange(-5, 5, 0.25)
+    ylen = len(Y)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X**2 + Y**2)
+    Z = np.sin(R)
+
+    colortuple = ('y', 'b')
+    colors = np.empty(X.shape, dtype=str)
+    for y in range(ylen):
+        for x in range(xlen):
+            colors[x, y] = colortuple[(x **2 ) % len(colortuple)]
+
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, facecolors=colors,
+                           linewidth=0, antialiased=False)
+
+    ax.set_zlim3d(-1, 1)
+    ax.w_zaxis.set_major_locator(LinearLocator(6))
+
+    plt.show()
+
+
+
+
+
 
 """
 1. Добавить загрузку названий функций в объект
 2. Визуализация 2d и 3d
 """
+
+
 
