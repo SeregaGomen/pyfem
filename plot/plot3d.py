53902f7f52492f576bb2a08a63fd3d51aedf0e46
SeregaGomen <gserega71@gmail.com>
2018-08-19 10:59:31 +0300

    Last

--- plot/plot3d.py
+++ plot/plot3d.py
@@ -29,12 +29,6 @@ class TMainWindow(QMainWindow):
         self.be = []                # ... ГЭ
         self.results = []           # Результаты расчета
 
-        # Пункты главного меню
-        self.file_menu = self.menuBar().addMenu('&File')
-        self.function_menu = self.menuBar().addMenu('F&unction')
-        self.options_menu = self.menuBar().addMenu('&Option')
-        self.help_menu = self.menuBar().addMenu('&?')
-
         self.__gl_widget__ = TGLWidget()
         self.setCentralWidget(self.__gl_widget__)
         self.resize(640, 480)
@@ -80,20 +74,25 @@ class TMainWindow(QMainWindow):
         return True
 
     def __init_main_menu__(self):
+        # Пункты главного меню
+        file_menu = self.menuBar().addMenu('&File')
+        function_menu = self.menuBar().addMenu('F&unction')
+        options_menu = self.menuBar().addMenu('&Option')
+        help_menu = self.menuBar().addMenu('&?')
         # Настройка File
         open_action = QAction('&Open...', self)
         open_action.setStatusTip('Open a data file')
         open_action.triggered.connect(self.__open_action__)
-        self.file_menu.addAction(open_action)
+        file_menu.addAction(open_action)
         close_action = QAction('&Close', self)
         close_action.setStatusTip('Close current file')
         close_action.triggered.connect(self.__close_action__)
-        self.file_menu.addAction(close_action)
-        self.file_menu.addSeparator()
+        file_menu.addAction(close_action)
+        file_menu.addSeparator()
         exit_action = QAction('E&xit', self)
         exit_action.setStatusTip('Exit application')
         exit_action.triggered.connect(self.close)
-        self.file_menu.addAction(exit_action)
+        file_menu.addAction(exit_action)
 
         # Настройка Function
         self.__init_function_menu__()
@@ -104,35 +103,35 @@ class TMainWindow(QMainWindow):
         light_action.triggered.connect(self.__light_action__)
         light_action.setCheckable(True)
         light_action.setChecked(True)
-        self.options_menu.addAction(light_action)
+        options_menu.addAction(light_action)
         fe_border_action = QAction('&FE border', self)
         fe_border_action.setStatusTip('Enable drawing FE border')
         fe_border_action.triggered.connect(self.__fe_border_action__)
         fe_border_action.setCheckable(True)
         fe_border_action.setChecked(False)
-        self.options_menu.addAction(fe_border_action)
+        options_menu.addAction(fe_border_action)
         legend_action = QAction('Le&gend', self)
         legend_action.setStatusTip('Enable drawing legend')
         legend_action.triggered.connect(self.__legend_action__)
         legend_action.setCheckable(True)
         legend_action.setChecked(True)
-        self.options_menu.addAction(legend_action)
-        self.options_menu.addSeparator()
+        options_menu.addAction(legend_action)
+        options_menu.addSeparator()
         invert_normal_action = QAction('&Invert normal', self)
         invert_normal_action.setStatusTip('Invert polygon normnal vector')
         invert_normal_action.triggered.connect(self.__invert_normal_action__)
         invert_normal_action.setCheckable(True)
         invert_normal_action.setChecked(False)
-        self.options_menu.addAction(invert_normal_action)
+        options_menu.addAction(invert_normal_action)
 
         light_two_side_action = QAction('&Two-sided lighting', self)
         light_two_side_action.setStatusTip('Invert polygon normnal vector')
         light_two_side_action.triggered.connect(self.__light_two_side_action__)
         light_two_side_action.setCheckable(True)
         light_two_side_action.setChecked(False)
-        self.options_menu.addAction(light_two_side_action)
+        options_menu.addAction(light_two_side_action)
 
-        self.options_menu.addSeparator()
+        options_menu.addSeparator()
         qa = QActionGroup(self)
         color_16_action = QAction('&16', self)
         color_16_action.setStatusTip('Set 16 grading colors')
@@ -172,7 +171,7 @@ class TMainWindow(QMainWindow):
         color_menu.addAction(color_64_action)
         color_menu.addAction(color_128_action)
         color_menu.addAction(color_256_action)
-        self.options_menu.addMenu(color_menu)
+        options_menu.addMenu(color_menu)
 
         self.show()
 
@@ -189,7 +188,7 @@ class TMainWindow(QMainWindow):
             function_action.setCheckable(True)
             function_action.setChecked(True if i == 0 else False)
             function_action.triggered.connect(self.__fun_action__)
-            self.function_menu.addAction(function_action)
+            self.menuBar().actions()[1].menu().addAction(function_action)
 
     def __open_action__(self):
         dlg = QFileDialog(self, 'Open data file', '', 'JSON data files (*.json)')
@@ -207,16 +206,17 @@ class TMainWindow(QMainWindow):
         self.be.clear()
         self.results.clear()
         self.__gl_widget__.clear()
-        self.function_menu.clear()
-        self.file_menu.actions()[1].setEnabled(False)   # Close menu
-        self.function_menu.setEnabled(False)
-        self.options_menu.setEnabled(False)
-        self.options_menu.actions()[0].setChecked(True)
-        self.options_menu.actions()[1].setChecked(False)
-        self.options_menu.actions()[2].setChecked(True)
-        self.options_menu.actions()[4].setChecked(False)
-        self.options_menu.actions()[5].setChecked(False)
-        self.options_menu.actions()[7].menu().actions()[0].setChecked(True)
+        # Сброс меню
+        self.menuBar().actions()[0].menu().actions()[1].setEnabled(False)   # Close
+        self.menuBar().actions()[1].menu().clear()                          # Очистка меню function
+        self.menuBar().actions()[1].setEnabled(False)                       # Function
+        self.menuBar().actions()[2].setEnabled(False)                       # Options
+        self.menuBar().actions()[2].menu().actions()[0].setChecked(True)
+        self.menuBar().actions()[2].menu().actions()[1].setChecked(False)
+        self.menuBar().actions()[2].menu().actions()[2].setChecked(True)
+        self.menuBar().actions()[2].menu().actions()[4].setChecked(False)
+        self.menuBar().actions()[2].menu().actions()[5].setChecked(False)
+        self.menuBar().actions()[2].menu().actions()[7].menu().actions()[0].setChecked(True)
 
     def __colors_action__(self):
         num = int(QObject.sender(self).text().replace('&', ''))
@@ -227,10 +227,10 @@ class TMainWindow(QMainWindow):
         self.__gl_widget__.set_results(self.results[index].results)
 
     def __light_action__(self):
-        self.options_menu.actions()[4].setEnabled(not self.options_menu.actions()[4].isEnabled())
-        self.options_menu.actions()[5].setEnabled(not self.options_menu.actions()[5].isEnabled())
-#        for i in range(0, len(self.__light_actions__)):
-#            self.__light_actions__[i].setEnabled(not self.__light_actions__[i].isEnabled())
+        self.menuBar().actions()[2].menu().actions()[4].setEnabled(not self.menuBar().actions()[2].
+                                                                   menu().actions()[4].isEnabled())
+        self.menuBar().actions()[2].menu().actions()[5].setEnabled(not self.menuBar().actions()[2].
+                                                                   menu().actions()[5].isEnabled())
         self.__gl_widget__.trigger_light()
         self.repaint()
 
@@ -269,9 +269,10 @@ class TMainWindow(QMainWindow):
             return
         self.__init_function_menu__()
         self.__gl_widget__.set_data(self.fe_type, self.x, self.fe, self.be, self.results[0].results)
-        self.file_menu.actions()[1].setEnabled(True)   # Close menu
-        self.function_menu.setEnabled(True)
-        self.options_menu.setEnabled(True)
+        # Настройка меню
+        self.menuBar().actions()[0].menu().actions()[1].setEnabled(True)
+        self.menuBar().actions()[1].setEnabled(True)                       # Function
+        self.menuBar().actions()[2].setEnabled(True)                       # Options
 
 
 # Базовый класс, реализующий основной функционал OpenGL
